e43e20fad4b5524e2600ed6a4e938ecda56ef9d2
gera <gera@satellogic.com>
2020-05-07 00:05:13 -0300

    Lots of changes, sorry: AutoConnect support for WiFi. OTA updates support

--- Alertame.ino
+++ Alertame.ino
@@ -1,19 +1,46 @@
 /* Adaptado de el ejemplo original por Cesar Riojas
     www.arduinocentermx.blogspot.mx
+    Autoconnect: https://www.hackster.io/hieromon-ikasamo/esp8266-esp32-connect-wifi-made-easy-d75f45
+        ESSID: esp8266ap / 12345678
  * */
-#include <ESP8266WiFi.h>
-#include <WiFiClientSecure.h>
-#include <UniversalTelegramBot.h>
+#define BOT_AND_WIFI
+#define AUTOCONNECT
 
+#include <ESP8266WiFi.h>
+#include <UniversalTelegramBot.h>
+#include <ArduinoOTA.h>
+#include <WiFiClientSecure.h>
+
+#ifdef AUTOCONNECT
+#include <AutoConnect.h>
+AutoConnect portal;
+#else
 // All WiFi
 
-char WiFi_ssid[] = "XXX";
-char WiFi_key[] = "XXX";
+//char WiFi_ssid[] = "Your ESSID";
+//char WiFi_key[] = "Your Password";
+#endif // AUTOCONNECT
+
 bool WiFi_ok = false;
+
+String my_name = "Caldera #1";
+
+bool input_read();
+
+// int digitalInputPin = 12;  // GPIO12 - NodemCU D6
+int digitalInputPin = 5;  // GPIO5 - Relay
 
 WiFiClientSecure client;
 
 void WiFi_setup() {
+#ifdef AUTOCONNECT
+  if (portal.begin()) {
+    Serial.println("connected:" + WiFi.SSID());
+    Serial.println("IP:" + WiFi.localIP().toString());
+  } else {
+    Serial.println("connection failed:");
+  }
+#else // AUTOCONNECT
   // Establecer el modo WiFi y desconectarse de un AP si fue Anteriormente conectada
   WiFi.mode(WIFI_STA);
   WiFi.disconnect();
@@ -21,17 +48,21 @@ void WiFi_setup() {
 
   Serial.println(WiFi_ssid);
   WiFi.begin(WiFi_ssid, WiFi_key);
+#endif // AUTOCONNECT
 
   WiFi_ok = WiFi.status() == WL_CONNECTED;
 }
 
 void WiFi_loop() {
+#ifdef AUTOCONNECT
+  portal.handleClient();
+#else // AUTOCONNECT
   static long lastCheck = 0;
   bool next_ok;
 
   if (millis() - lastCheck > 500) {
     next_ok = WiFi.status() == WL_CONNECTED;
-    
+
     if (!next_ok) {
       Serial.print(".");
     }
@@ -42,6 +73,7 @@ void WiFi_loop() {
     WiFi_ok = next_ok;
     lastCheck = millis();
   }
+#endif // AUTOCONNECT
 }
 
 // Blinker
@@ -54,11 +86,12 @@ void cmd_blink() {
 
 void cmd_unblink() {
   blinking = false;
-  Serial.begin(115200, SERIAL_8N1); 
+  Serial.begin(115200, SERIAL_8N1);
 }
 
 void blink_setup() {
-  Serial.begin(115200, SERIAL_8N1);
+  cmd_unblink();
+  Serial.println("\nI'm " + my_name + "\n");
 }
 
 void blink_loop() {
@@ -79,15 +112,27 @@ void blink_loop() {
 
 UniversalTelegramBot bot(BOTtoken, client);
 
-String default_chat_id = "25235518";
+String default_chat_id = "25235518"; // gera
+// String default_chat_id = "268186747"; // Agu
+
 int Bot_mtbs_ms = 5000;
 long Bot_nexttime = 0;
 bool Bot_greeted = false;
 
 // Greeter
 void cmd_hola(String chat_id, String from_name) {
-  String welcome = "Hola, " + from_name + ". chat_id=" + chat_id + "\n";
+  String welcome = "Hola, " + from_name + ". chat_id=" + chat_id + ". I'm "+ my_name +"\n";
   bot.sendMessage(chat_id, welcome);
+  default_chat_id = chat_id;
+}
+
+void cmd_status(String chat_id, String from_name) {
+  String st = input_read()?"Ok!":"ALARMA!";
+  String msg = my_name + " status: " + st + "\n";
+  
+  if (chat_id == "") chat_id = default_chat_id;
+  Serial.print(msg);
+  bot.sendMessage(chat_id, msg);
 }
 
 void Bot_handleNewMessages(int numNewMessages) {
@@ -100,9 +145,15 @@ void Bot_handleNewMessages(int numNewMessages) {
     cmd.toLowerCase();
     if (from_name == "") from_name = "GUEST";
 
-    if (cmd == "hola") cmd_hola(chat_id, from_name);
-    if (cmd == "blink") cmd_blink();
-    if (cmd == "unblink") cmd_unblink();
+    Serial.print("Received \"" + cmd + "\" from " + from_name + "\n");
+
+    if (cmd == "hola") {
+      cmd_hola(chat_id, from_name);
+      cmd_status(chat_id, from_name);
+    }
+//    if (cmd == "blink") cmd_blink();
+//    if (cmd == "unblink") cmd_unblink();
+    if (cmd == "status") cmd_status(chat_id, from_name);
     if (!firstMsg && cmd == "reset") ESP.reset();
     firstMsg = false;
   }
@@ -118,7 +169,7 @@ void Bot_loop() {
   if (!Bot_greeted) {
     Bot_greeted = true;
     if (default_chat_id != "") {
-      bot.sendMessage(default_chat_id, "HOLA!!!");
+      cmd_hola(default_chat_id, "?");
     }
   }
   if (millis() > Bot_nexttime)  {
@@ -132,18 +183,98 @@ void Bot_loop() {
       Bot_handleNewMessages(numNewMessages);
       numNewMessages = bot.getUpdates(bot.last_message_received + 1);
     }
-    Bot_nexttime = millis()+Bot_mtbs_ms;
+    Bot_nexttime = millis() + Bot_mtbs_ms;
   }
 }
 
+///////////////////////
+
+bool input_status = false;
+
+void input_setup() {
+  pinMode(digitalInputPin, INPUT_PULLUP);
+  input_status = digitalRead(digitalInputPin);
+}
+
+bool input_read() {
+  return (digitalRead(digitalInputPin));
+}
+
+void input_loop() {
+  bool new_status = digitalRead(digitalInputPin);
+
+  
+  if (input_status != new_status) {
+  // Serial.print(String(new_status)+"\r");
+#ifdef BOT_AND_WIFI
+      cmd_status("","");
+#endif
+      input_status = new_status;
+  }
+}
+
+//// OTA
+
+void OTA_setup() {
+  ArduinoOTA.setPort(8266);
+  ArduinoOTA.setHostname("caldera1");
+  ArduinoOTA.setPasswordHash("fff9a16a632c7daa86f7a4a8ce1929d6");
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_FS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
+void OTA_loop() {
+  ArduinoOTA.handle();
+}
+
+///////////////////////
+
 void setup() {
   blink_setup();
+#ifdef BOT_AND_WIFI
   WiFi_setup();
+  OTA_setup();
   Bot_setup();
+#endif
+  input_setup();
 }
 
 void loop() {
+#ifdef BOT_AND_WIFI
   WiFi_loop();
+  OTA_loop();
   Bot_loop();
-  blink_loop();
+#endif
+  // blink_loop();
+  input_loop();
 }
